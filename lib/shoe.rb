23641a582937bfdf74624704786a7cd5e6d41f3e
Gina Graci <ginagraci92@gmail.com>
2019-10-27 13:39:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :brand, :color, :size, :material
   attr_reader :title
  
   def initialize(brand)
     @brand = brand
   end
  
-  def turn_page
+  def cobble
     puts "Flipping the page...wow, you read fast!"
   end
  
